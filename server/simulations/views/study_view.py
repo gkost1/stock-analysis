cc4d92d5ac81da2698c972a6a85be5d9c49f62d0
garrettkost <gkost3@gatech.edu>
2026-07-27 13:34:02 -0500

    add permission check to study view and update testing infra

--- server/simulations/views/study_view.py
+++ server/simulations/views/study_view.py
@@ -1,16 +1,18 @@
-from rest_framework import mixins, viewsets
+from rest_framework import permissions, viewsets
 
 from simulations.models import Study
 from simulations.serializers import StudySerializer
 
 
+class _CanViewStudy(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.created_by == request.user
+
 class StudyViewSet(
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
+    viewsets.ModelViewSet
 ):
     serializer_class = StudySerializer
+    permission_classes = [_CanViewStudy]
 
     def get_queryset(self):
         return Study.objects.filter(created_by=self.request.user)
